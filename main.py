b43beb5ac076c107cbd235b24d30ce19f2e8c6c2
jamcris11 <jamcris11@hotmail.co.uk>
2021-08-24 20:35:42 +0100

    Alt: move 4chan specific function to utils

--- main.py
+++ main.py
@@ -11,40 +11,14 @@ from bs4 import BeautifulSoup
 import args as ARG
 import html_parsing as HTML
 import util as UTIL
+import download as DL
 
 #-------------------------------------------------------------------------------
-
-def download_file(url, dest):
-    urllib.request.urlretrieve("https://" + url, dest)
-    print(url + "\t-- DONE")
-
-
-def download_files(urls, dir):
-    for url in urls:
-        download_file(str(url), dir + '/' + UTIL.file_name_from_url(url))
-
-
-def download_files_multithreaded(urls, dir):
-    t1_urls, t2_urls = UTIL.split_array(urls)
-
-    t1 = threading.Thread(target=download_files, args = (t1_urls, dir))
-    t2 = threading.Thread(target=download_files, args = (t2_urls, dir))
-
-    t1.start()
-    t2.start()
-
-    t1.join()
-    t2.join()
-
-
-def four_chan_get_urls(html_parser):
-    return HTML.parse_urls(html_parser.find_all(class_="fileThumb"))
-
 
 def start_thread_download(url, dir, single_threaded=False):
     req = requests.get(url, auth=('user', 'pass'))
     html_parser = BeautifulSoup(req.text, 'html.parser')
-    thread_file_urls = four_chan_get_urls(html_parser)
+    thread_file_urls = UTIL.four_chan_get_urls(html_parser)
 
 
     if (not os.path.exists(dir)):
--- util.py
+++ util.py
@@ -1,3 +1,5 @@
+import html_parsing as HTML
+
 # Splits the array into two so that both threads can handle a portion
 def split_array(array):
     midpoint = len(array)//2
@@ -6,3 +8,7 @@ def split_array(array):
 
 def file_name_from_url(url):
 	return url[url.rfind("/")+1:]
+
+
+def four_chan_get_urls(html_parser):
+    return HTML.parse_urls(html_parser.find_all(class_="fileThumb"))
